185b2ca1cb664b120aba314cd5c35040bd285cb4
sarelknoetze <122682873+sarelknoetze@users.noreply.github.com>
2023-02-20 18:33:18 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,76 @@
+
+$(".hover1").hover(function(){
+    $(".heroSection").css({"background-image": "url(./images/try.jpg)", "background-position": "center", "color": "black"})
+});
+
+$(".hover1").mouseleave(function () {
+    $(".heroSection").css({"background-image": "none", "color": "white"})
+});
+
+$(".hover2").hover(function(){
+    $(".heroSection").css({"background-position": "center", "color": "black"})//sit jou foto in vir here section background
+});
+
+$(".hover2").mouseleave(function () {
+    $(".heroSection").css({"background-image": "none", "color": "white"})
+});
+
+$(".hover3").hover(function(){
+    $(".heroSection").css({"background-position": "center", "color": "black"})//sit jou foto in vir here section background
+});
+
+$(".hover3").mouseleave(function () {
+    $(".heroSection").css({"background-image": "none", "color": "white"})
+});
+
+$(".hover4").hover(function(){
+    $(".heroSection").css({"background-position": "center", "color": "black"})//sit jou foto in vir here section background
+});
+
+$(".hover4").mouseleave(function () {
+    $(".heroSection").css({"background-image": "none", "color": "white"})
+});
+
+$(".hover5").hover(function(){
+    $(".heroSection").css({"background-position": "center", "color": "black"})//sit jou foto in vir here section background
+});
+
+$(".hover5").mouseleave(function () {
+    $(".heroSection").css({"background-image": "none", "color": "white"})
+});
+
+$(".hover6").hover(function(){
+    $(".heroSection").css({"background-position": "center", "color": "black"})//sit jou foto in vir here section background
+});
+
+$(".hover6").mouseleave(function () {
+    $(".heroSection").css({"background-image": "none", "color": "white"})
+});
+
+$(".hover7").hover(function(){
+    $(".heroSection").css({"background-position": "center", "color": "black"})//sit jou foto in vir here section background
+});
+
+$(".hover7").mouseleave(function () {
+    $(".heroSection").css({"background-image": "none", "color": "white"})
+});
+
+$(".hover8").hover(function(){
+    $(".heroSection").css({"background-position": "center", "color": "black"})//sit jou foto in vir here section background
+});
+
+$(".hover8").mouseleave(function () {
+    $(".heroSection").css({"background-image": "none", "color": "white"})
+});
+
+$(".hover9").hover(function(){
+    $(".heroSection").css({"background-position": "center", "color": "black"})//sit jou foto in vir here section background
+});
+
+$(".hover9").mouseleave(function () {
+    $(".heroSection").css({"background-image": "none", "color": "white"})
+});
+
+
+
+// maak n function vir waneur hulle se dat hulle het op company/personal geclick sodat dit company naam gee of personal detalis
